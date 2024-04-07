ac2580f672c28725f2931edaae39a43fef19eb49
christophergz <166271705+christophergz@users.noreply.github.com>
2024-04-07 11:28:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel=”icon” href=”hasan.png”>
+		<link href="https://mynameisrosita.nasiwebhost.com/profile/snakesec.png" rel="icon" type="image/x-icon">
+		<meta property="og:image" content="https://mynameisrosita.nasiwebhost.com/profile/snakesec.png">
+		<meta http-equiv="cache-control" content="index,cache">
+		<meta http-equiv="pragma" content="index,cache">
+		<meta charset="utf-8">
+    <title>Christopher</title>
+		<link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet">
+		<link rel="stylesheet" href="style.css">
+		<br>
+    <embed src="https://youtu.be/AnMhdn0wJ4I&autoplay=1&start=0" type="application/x-shockwave-flash" wmode="transparent" width="0" height="0"></embed>
+    <script tipe="text/javascript"src="http://htmlfreecodes.com/codes/rain.js"></script>
+<style>
+    .container {
+        max-width: 600px;
+        margin: 50px auto;
+    }
+    .comment-section {
+        margin-top: 20px;
+    }
+</style>
+<style>
+    .profil-container {
+        display: ;
+        align-items: center;
+        font-family: Arial, sans-serif;
+    }
+
+    .profile-img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+
+    .profile-info {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .profile-info h6 {
+        margin: 0;
+        font-size: 16px;
+        font-weight: bold;
+    }
+
+    .profile-info .recent-chat {
+        margin: 0;
+        font-size: 14px;
+    }
+</style>
+<style>
+    .navbar {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 60px;
+        background-color: rgba(0, 0, 0, 0.5); /* Warna hitam transparan */
+        text-align: center;
+        color: #fff; /* Warna teks putih */
+    }
+    .navbar a {
+        color: #fff;
+        text-decoration: none;
+        margin: 0 15px;
+    }
+</style>
+<link rel="stylesheet" href="style.css">
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+    <div id="mobile" class="mobile">
+      <center><font size="5" color="red">Author By : </font><font size="5" color="gray">Christopher</font><br><br></center>
+	<center><img style="border-radius: 50%;" title="Hasan GZ" src="hasan.png" width="100"><br><br></center>
+	<i>
+	<center><font size="5" color="aqqua">No System Is Safe</font><br><br>
+	<font size="6" color="red">CYBER</font><br>
+      <font size="2.7" color="gray">Adalah Dunia Maya Dimana Kita Bisa Berkreasi & Menjelajahi Isinya.Sayangnya Hobi Kami Di Anggap Asing/Aneh Oleh Mereka Padahal Mereka Sendiri Belum Tau Asal Mula Internet Di Ciptakan Itu Bagaimana !</font><br><br><br><br>
+    <font size="5" color="red">&nbsp;Creator By<br>Chrish</font><br><hr><br></center>
+    <center><font size="4" color="coral">[ YOUTUBE CHANNEL ]</font></center><br>
+	<hr><div class="footer-greetings"><marquee><font size="4" color="red">
+		<b>NOTE  </b> : </font><font size="3" color="gray">
+       Saya adalah Salah Satu Youtuber di Indonesia, Founder Channel Christopher, Content Creator, Founder Website Christopher.com</font></font></marquee></div></td></table>
+		<hr>
+		<div class="footer-greetings"><marquee><font size="4" color="gray">
+		<b>LINK CHANNEL </b> : </font><font size="3" color="red">https://www.youtube.com/@Christopher_875</font></font></marquee></div></td></table>
+		<hr><br>
+	</i>
+      <audio style="margin-left: 5px; width: 165px; height: 40px;" controls="controls" autoplay="true" loop="loop" src="djrasahbali.mp3"></audio>
+		<audio style="width: 165px; height: 40px;" controls="controls" autoplay="true" loop="loop" src="djweslalekekabeh.mp3"></audio>
+      <div class="container">
+        <h2 style="color: gray;">Komentar</h2>
+        <form action="proses.php" method="POST">
+          <div style="justify-content: center; text-align: center;">
+            <input style="outline: none; border: 1.9px solid red; width: 300px; height: 35px; font-size: 16px; border-radius: 10px;" type="text" id="nama" name="nama" placeholder="Masukkan nama anda" required><br><br>
+            <input style="outline: none; border: 1.9px solid red; width: 250px; margin-left: -50px; font-size: 16px; height: 35px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;" id="komentar" name="komentar" rows="4" placeholder="Apa yang anda pikirkan?" required>
+            <input style="float: left; width: 50px; margin-top: -40px; height: 40px; background-color: red; border: 1.9px solid red; color: white; border-radius: 0 10px 10px 0; margin-left: 275px;" type="submit" value="Kirim" name="submit"></div>
+        </form>
+        <div class="comment-section">
+            <h3 style="color: gray;">Komentar Terbaru:</h3>
+            <?php include 'display.php'; ?>
+    </div>
+        </div>
+    </div><br><br><br><br><br>
+    <div class="navbar">
+         <center><p>Copyright &copy; All Right Reserved | Designed <span>By Christopher</span> 2024</p></center>
+    </div>
+    </div>
+    <div id="desktop" class="desktop">
+      <center><font size="5" color="red">Author By : </font><font size="5" color="gray">Christopher</font><br><br></center>
+	<center><img style="border-radius: 50%;" title="Hasan GZ" src="hasan.png" width="100"><br><br></center>
+	<i>
+	<center><font size="5" color="aqqua">No System Is Safe</font><br><br>
+	<font size="6" color="red">CYBER</font><br>
+      <font size="2.7" color="gray">Adalah Dunia Maya Dimana Kita Bisa Berkreasi & Menjelajahi Isinya.Sayangnya Hobi Kami Di Anggap Asing/Aneh Oleh Mereka Padahal Mereka Sendiri Belum Tau Asal Mula Internet Di Ciptakan Itu Bagaimana !</font><br><br><br><br>
+    <font size="5" color="red">&nbsp;Creator By<br>Chrish</font><br><hr><br></center>
+    <center><font size="4" color="coral">[ YOUTUBE CHANNEL ]</font></center><br>
+	<hr><div class="footer-greetings"><marquee><font size="4" color="red">
+		<b>NOTE  </b> : </font><font size="3" color="gray">
+       Saya adalah Salah Satu Youtuber di Indonesia, Founder Channel Christopher, Content Creator, Founder Website Christopher.com</font></font></marquee></div></td></table>
+		<hr>
+		<div class="footer-greetings"><marquee><font size="4" color="gray">
+		<b>LINK CHANNEL </b> : </font><font size="3" color="red">https://www.youtube.com/@Christopher_875</font></font></marquee></div></td></table>
+		<hr><br>
+	</i>
+      <audio style="margin-left: 5px; width: 165px; height: 40px;" controls="controls" autoplay="true" loop="loop" src="djrasahbali.mp3"></audio>
+		<audio style="width: 165px; height: 40px;" controls="controls" autoplay="true" loop="loop" src="djweslalekekabeh.mp3"></audio>
+      <div class="container">
+        <h2 style="color: gray;">Komentar</h2>
+        <form action="proses.php" method="POST">
+          <div style="justify-content: center; text-align: center;">
+            <input style="outline: none; border: 1.9px solid red; width: 300px; height: 35px; font-size: 16px; border-radius: 10px;" type="text" id="nama" name="nama" placeholder="Masukkan nama anda" required><br><br>
+            <input style="outline: none; border: 1.9px solid red; width: 250px; margin-left: -50px; font-size: 16px; height: 35px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;" id="komentar" name="komentar" rows="4" placeholder="Apa yang anda pikirkan?" required>
+            <input style="float: left; width: 50px; margin-top: -40px; height: 40px; background-color: red; border: 1.9px solid red; color: white; border-radius: 0 10px 10px 0; margin-left: 403px;" type="submit" value="Kirim" name="submit"></div>
+        </form>
+        <div class="comment-section">
+            <h3 style="color: gray;">Komentar Terbaru:</h3>
+            <?php include 'display.php'; ?>
+    </div>
+        </div>
+    </div><br><br><br><br><br>
+    <div class="navbar">
+         <center><p>Copyright &copy; All Right Reserved | Designed <span>By Christopher</span> 2024</p></center>
+    </div>
+    </div>
+</body>
+</html>
